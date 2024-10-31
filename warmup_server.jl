37c5ca101c5910e6b5ca93b75eb67f068b52ef10
guoyongzhi <guo-yong-zhi@outlook.com>
2024-10-31 16:48:06 +0800

    move CondaPkg installation to server warmup

--- warmup_server.jl
+++ warmup_server.jl
@@ -5,6 +5,8 @@ Pkg.instantiate()
 @info("import Pluto from setup.py")
 import Pluto
 
+using CondaPkg; CondaPkg.add("jieba")
+
 @info("starting new notebook from setup.py")
 sesh = Pluto.ServerSession(options=Pluto.Configuration.from_flat_kwargs(; include("pluto_server_config.jl")...))
 nb = Pluto.SessionActions.new(sesh; run_async=false)
